990645ff1043f38381670f157e195d7ba2b49067
putrihsnh <putrihasanah30@mhs.mdp.ac.id>
2022-10-03 11:20:39 +0700

    Pindah Layout

--- app/src/main/java/com/putripab1/pindahlayout/MainActivity.java
+++ app/src/main/java/com/putripab1/pindahlayout/MainActivity.java
@@ -6,14 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button getBtnPindah;
+    EditText etNama;
 
     Button btnPindah;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("JANGAN LUPA SENYUM HARI INI PUT ");
 
         btnPindah = findViewById(R.id.btn_pindah);
         btnPindah.setOnClickListener(new View.OnClickListener() {
